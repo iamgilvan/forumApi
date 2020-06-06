78509bd883ba7cb67c69d2871f5b295237fe0d5f
Gilvan Almeida <gilvan.praxedes@outlook.com>
2020-06-05 22:38:50 -0300

    Proteção com spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2y$12$MQjo2JfI7xJa/FUbognGreXkWzThgsRcug4JwRyaapU1KeQbCWzOW');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
